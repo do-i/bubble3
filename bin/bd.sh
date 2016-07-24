c996ea758780fb261a98de7713942598b4ccb76e
Ninja <ninja@renton.net>
2016-07-24 07:14:09 -0500

    added clean option

--- bin/bd.sh
+++ bin/bd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # This is build and deploy script
-
+if [ "$1" == "clean" ]; then
+  BIN_DIR=$(pwd)
+  WORK_DIR=$(dirname "${BIN_DIR}")
+  rm ${WORK_DIR}/tmp/*.tgz
+fi
 ./build.sh && ./deploy.sh
