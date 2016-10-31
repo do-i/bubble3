8d317e0d7e18ebe66921a5234e0075917b83e6dd
Ninja <ninja@renton.net>
2016-10-30 22:42:12 -0500

    webixnize the ui  * TODO still need to impl event handler

--- web/js/bubble.js
+++ web/js/bubble.js
@@ -7,50 +7,86 @@
  * Load media_list from the json file
  */
 $.getJSON("data/media_files_list.json", function(result) {
-  if (result.hasOwnProperty("videos")) {
-    $.each(result["videos"]["files"], function(i, item) {
-      var btnDiv = $("<div></div>", {
-        "class": "col-xs-12 col-sm-6 col-md-4 col-lg-3 top-margin-1"
-      }).appendTo("#files_list");
+  console.log(result);
+  webix.ui({
+    margin: 10,
+    padding: 0,
+    type: "wide",
+    view: "flexlayout",
+    cols: [{
+      container: "media_list_poc",
+      view: "grouplist",
+      templateBack: " Category #category#",
+      templateGroup: " Category #value#",
+      templateItem: "#title#",
+      select: true,
+      scheme: {
+        $group: {
+          by: 'category'
+        },
+        $sort: {
+          by: "value",
+          dir: "desc"
+        }
+      },
+      on: {
+        onSelectChange: function() {
+          var media_item = this.getSelectedItem();
+          console.log(media_item);
+          console.log(media_item.title);
+          console.log(media_item.file_ext);
+          console.log(media_item.dir);
+        }
+      },
+      data: webix.copy(result)
+    }]
+  });
 
-      var btn = $("<button></button>", {
-        "type": "button",
-        "class": "btn btn-bubble btn-lg btn-block",
-        "data-toggle": "modal",
-        "data-target": "#video_modal",
-        "value": item
-      }).appendTo(btnDiv);
-      btn.html(item.substring(0, item.lastIndexOf('.')));
-      btn.on("click", function() {
-        var selection = $(this).val();
-        $("#video_elm").empty(); // clear previous source element
-        var video_src = $("<source/>", {
-          "src": "ext-content/" + result['videos']['dir'] + '/' + selection,
-          "type": "video/mp4"
-        }).appendTo("#video_elm");
-        $("#video_elm").load();
-      });
-    });
-  } // end if json has videos section
-
-  if (result.hasOwnProperty("documents")) {
-    $.each(result["documents"]["files"], function(i, item) {
-      var btnDiv = $("<div></div>", {
-        "class": "col-xs-12 col-sm-6 col-md-4 col-lg-3 top-margin-1"
-      }).appendTo("#files_list");
-
-      var btn = $("<button></button>", {
-        "type": "button",
-        "class": "btn btn-bubble btn-lg btn-block",
-        "value": item
-      }).appendTo(btnDiv);
-      btn.html(item.substring(0, item.lastIndexOf('.')));
-      btn.on("click", function() {
-        var selection = $(this).val();
-        window.location = "ext-content/" + result['documents']['dir'] + '/' + selection;
-      });
-    });
-  } // end if json has documents section
+  //
+  // if (result.hasOwnProperty("videos")) {
+  //   $.each(result["videos"]["files"], function(i, item) {
+  //     var btnDiv = $("<div></div>", {
+  //       "class": "col-xs-12 col-sm-6 col-md-4 col-lg-3 top-margin-1"
+  //     }).appendTo("#files_list");
+  //
+  //     var btn = $("<button></button>", {
+  //       "type": "button",
+  //       "class": "btn btn-bubble btn-lg btn-block",
+  //       "data-toggle": "modal",
+  //       "data-target": "#video_modal",
+  //       "value": item
+  //     }).appendTo(btnDiv);
+  //     btn.html(item.substring(0, item.lastIndexOf('.')));
+  //     btn.on("click", function() {
+  //       var selection = $(this).val();
+  //       $("#video_elm").empty(); // clear previous source element
+  //       var video_src = $("<source/>", {
+  //         "src": "ext-content/" + result['videos']['dir'] + '/' + selection,
+  //         "type": "video/mp4"
+  //       }).appendTo("#video_elm");
+  //       $("#video_elm").load();
+  //     });
+  //   });
+  // } // end if json has videos section
+  //
+  // if (result.hasOwnProperty("documents")) {
+  //   $.each(result["documents"]["files"], function(i, item) {
+  //     var btnDiv = $("<div></div>", {
+  //       "class": "col-xs-12 col-sm-6 col-md-4 col-lg-3 top-margin-1"
+  //     }).appendTo("#files_list");
+  //
+  //     var btn = $("<button></button>", {
+  //       "type": "button",
+  //       "class": "btn btn-bubble btn-lg btn-block",
+  //       "value": item
+  //     }).appendTo(btnDiv);
+  //     btn.html(item.substring(0, item.lastIndexOf('.')));
+  //     btn.on("click", function() {
+  //       var selection = $(this).val();
+  //       window.location = "ext-content/" + result['documents']['dir'] + '/' + selection;
+  //     });
+  //   });
+  // } // end if json has documents section
 }); // end of getJSON()
 
 /*
