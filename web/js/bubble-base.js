29084a33241cc213e2b2d9c98d30f71eb52cdd54
Ninja <ninja@renton.net>
2016-11-23 18:45:02 -0600

    update each page to use supersized plugin

---
+++ web/js/bubble-base.js
@@ -0,0 +1,13 @@
+/*!
+ * Copyright 2016 Joji Doi
+ * Licensed under the MIT license
+ */
+
+$(function() {
+  $.supersized({
+    slides: [{
+      image: '../img/background.jpg',
+      title: 'Custom Be G'
+    }]
+  });
+});
