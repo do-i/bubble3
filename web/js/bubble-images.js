f7e8c7a747c133a794c7288820b4f00df326e237
Ninja <ninja@renton.net>
2017-02-17 18:34:42 -0600

    [Optimization] Use lazyLoadChunks API to load small chunks at a time

--- web/js/bubble-images.js
+++ web/js/bubble-images.js
@@ -20,20 +20,23 @@ $.getJSON("data/media_files_list.json", function(result) {
   var mediaFiles = $(result).filter(function() {
     return this.category == "photos";
   });
+  var datas = [];
   $.each(mediaFiles, function(i, mediaItem) {
-    var aTag = $("<a/>", {
-      "href": getMediaFilePath(mediaItem)
+    datas.push({
+      image: getMediaFilePath(mediaItem),
+      thumb: getMediaThumbFilePath(mediaItem)
     });
-    var img = $("<img/>", {
-      "src": getMediaThumbFilePath(mediaItem)
-    });
-    img.appendTo(aTag);
-    aTag.appendTo("#files_list");
   });
-  $(".galleria").galleria({
+  Galleria.run('.galleria', {
+    thumbnails: "lazy",
+    dataSource: datas,
     responsive: true,
-    height: 0.5
+    height: 0.5,
+    autoplay: true
   });
-  Galleria.run('.galleria');
 
+  // lazy load small chunks of thumbnails at a time
+  Galleria.ready(function() {
+    this.lazyLoadChunks(30, 1000); // 30 thumbs per second
+  })
 }); // end of getJSON()
