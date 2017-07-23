832e31826a14bc4211f1c7d58908316f8044a1bc
Ninja <ninja@renton.net>
2017-07-23 14:37:23 -0500

    [UIv2/web] Add UI changes made by Greg

--- web/js/bubble3.js
+++ web/js/bubble3.js
@@ -1,71 +1,48 @@
 /*!
- * Copyright 2016 Joji Doi
+ * Copyright 2017 Joji Doi, Greg Mendez
+
  * Licensed under the MIT license
  */
+var jsonResult;
 
-function getMediaFilePath(mediaItem) {
-  return "ext-content/" + mediaItem.dir + '/' + mediaItem.title +
-    mediaItem.file_ext;
+function getMediaFilePath(mediaItem, dirName) {
+  return "ext-content" + dirName + "/" + mediaItem.name;
 }
 
-/*
- * Parameter -- iconClass is a string name of fontawesome class name.
- */
-function createIconSpan(iconClass) {
-  return $('<span/>', {
-    "class": "webix_icon"
-  }).addClass(iconClass).get(0).outerHTML;
+function getExt(mediaItem) {
+  var end = mediaItem.name.toString().toLowerCase();
+  var ind = end.lastIndexOf(".");
+  end = end.substring(ind);
+  return end;
 }
 
-function renderMedia(headerTitle, mediaElement, onCloseAction) {
-  webix.ui({
-    view: "window",
-    id: "media_window",
-    fullscreen: true,
-    head: {
-      view: "toolbar",
-      margin: -10,
-      cols: [{
-        view: "label",
-        label: headerTitle
-      }, {
-        view: "icon",
-        icon: "times-circle",
-        click: function() {
-          onCloseAction();
-          $$('media_window').close();
-        }
-      }]
-    },
-    body: {
-      padding: 0,
-      rows: [{
-        // Ensures URL encoding is applied to the src URL
-        template: mediaElement.get(0).outerHTML
-      }]
-    }
-  }).show();
-}
-
-function renderPdf(mediaItem) {
+function renderPdf(mediaItem, dir) {
   // Note embed nor iframe works great to render pdf. So, this is workaround until better alternative is found.
-  window.location = getMediaFilePath(mediaItem);
+  //window.location = getMediaFilePath(mediaItem, dir);
+  $(".bubble-audio-title").text(mediaItem.name);
+  $("#pdf_elm").empty(); // clear previous source element
+  var pdf_src = $("<iframe></iframe>", {
+    "src": getMediaFilePath(mediaItem, dir),
+    "type": "application/pdf",
+    "class": "txt",
+  }).appendTo("#pdf_elm");
+  $("#pdf_elm").load();
 }
 
-function renderText(mediaItem) {
-  renderMedia(mediaItem.title, $("<iframe/>", {
-    id: "iframe_elm",
-    src: getMediaFilePath(mediaItem),
-    type: "application/text",
-    width: "100%",
-    height: "100%"
-  }), function() {
-    // no-op
-  });
+function renderText(mediaItem, dir) {
+  $(".bubble-audio-title").text(mediaItem.name);
+  $("#pdf_elm").empty(); // clear previous source element
+  var pdf_src = $("<iframe></iframe>", {
+    "src": getMediaFilePath(mediaItem, dir),
+    "class": "txt",
+    "height": "50em",
+    "width": "100%"
+  }).appendTo("#pdf_elm");
+  $("#pdf_elm").load();
 }
 
 function renderDoc(mediaItem) {
-  var mediaType = mediaItem.file_ext.toString().toLowerCase();
+  var mediaType = getExt(mediaItem);
   switch (mediaType) {
     case ".pdf":
       renderPdf(mediaItem);
@@ -78,35 +55,39 @@ function renderDoc(mediaItem) {
   }
 }
 
-function renderAudio(mediaItem) {
-  renderMedia(mediaItem.title, $("<audio/>", {
-    id: "audio_elm",
-    src: getMediaFilePath(mediaItem),
-    type: "audio/mp3", // TODO  mediaItem.file_ext.toString().substring(1).toLowerCase();
-    width: "100%",
-    controls: true
-  }), function() {
-    $("#audio_elm")[0].pause();
-  });
+function renderAudio(mediaItem, dir) {
+  $(".bubble-audio-title").text(mediaItem.name);
+  $("#audio_elm").empty(); // clear previous source element
+  var type = getExt(mediaItem).substring(1);
+  var video_src = $("<source></source>", {
+    "src": getMediaFilePath(mediaItem, dir),
+    "type": "audio/" + type
+  }).appendTo("#audio_elm");
+  $("#audio_elm").load();
 }
 
-function renderVideo(mediaItem) {
-  renderMedia(mediaItem.title, $("<video/>", {
-    id: "video_elm",
-    src: getMediaFilePath(mediaItem),
-    type: "video/mp4", // TODO  mediaItem.file_ext.toString().substring(1).toLowerCase();
-    width: "100%",
-    controls: true
-  }), function() {
-    $("#video_elm")[0].pause();
-  });
+function renderVideo(mediaItem, dir) {
+  var type = getExt(mediaItem).substring(1);
+  $("#video_elm").empty(); // clear previous source element
+  var sour = $("<source></source>", {
+    "src": getMediaFilePath(mediaItem, dir),
+    "type": "video/" + type,
+  }).appendTo("#video_elm");
+  $("#video_elm").load();
 }
 
+function renderPhoto(mediaItem, dir) {
+  $("#img_elm").empty(); // clear previous source element
+  var video_src = $("<img/>", {
+    "src": getMediaFilePath(mediaItem, dir),
+    "class": "modal-content"
+  }).appendTo("#img_elm");
+  $("#img_elm").load();
+}
 //
 // Photo viewer is still under construction.
 //
-
-function renderPhoto(mediaItem) {
+function renderPhotoOld(mediaItem) {
   webix.ui({
     view: "window",
     id: "image_window",
@@ -135,7 +116,6 @@ function renderPhoto(mediaItem) {
   $$("bubble_carousel").setActive(mediaItem.id);
   $$("image_window").show();
 }
-
 /*
  * Retrieve cached image file path data.
  * See cacheImageFilePaths(media_file_list) function for cache creation.
@@ -143,116 +123,200 @@ function renderPhoto(mediaItem) {
 function getImageFilePathsFromCache() {
   return $("body").data("image file list");
 }
-
 /*
  * Save image file paths data in cache for later reuse via getImageFilePathsFromCache() function.
  */
 function cacheImageFilePaths(media_file_list) {
   function img(obj) {
-    return '<img src="' + obj.src +
-      '" class="content" ondragstart="return false"/><div class="title">' + obj.title +
-      '</div>';
+    return '<img src="' + obj.src + '" class="content" ondragstart="return false"/><div class="title">' + obj.name + '</div>';
   }
   console.log("This should be called only once.");
   var imageFilePaths = [];
   media_file_list.forEach(function(media_file) {
-    if (media_file.category.toString().toLowerCase() == "photos") {
+    if (media_file.type.toString().toLowerCase() == "photos") {
       imageFilePaths.push({
         id: media_file.id,
         css: "image",
         template: img,
         data: webix.copy({
           src: getMediaFilePath(media_file),
-          title: media_file.title
+          title: media_file.name
         })
       });
     }
   });
   $("body").data("image file list", imageFilePaths);
 }
+var folderStack = [];
 
-function getIconClass(mediaItem) {
-  var faIconClass;
-  switch (mediaItem.file_ext.toString().toLowerCase()) {
-    case ".pdf":
-      faIconClass = "fa-file-pdf-o";
-      break;
-    case ".txt":
-      faIconClass = "fa-file-text-o";
-      break;
-    case ".mp4":
-    case ".webm":
-      faIconClass = "fa-file-movie-o";
-      break;
-    case ".mp3":
-    case ".ogg":
-      faIconClass = "fa-file-audio-o";
-      break;
-    case ".png":
-    case ".jpg":
-      faIconClass = "fa-file-picture-o";
-      break;
-    default:
-      faIconClass = "fa-file";
+function renderMediaDynamic(mediaItem, title) {
+  if (mediaItem.type == "directory") {
+    var opened = false;
+    var pos;
+    var elementIcon;
+    var element;
+    var len = folderStack.length;
+    elementIcon = document.getElementById(mediaItem.name + "Icon");
+    for (var i = 0; i < folderStack.length; i++) {
+      if (elementIcon.parentElement.parentElement.id + "/" + mediaItem.name == folderStack[i]) {
+        opened = true;
+        pos = i;
+      }
+    }
+    if (opened) {
+      for (var i = pos; i < len; i++) {
+        element = document.getElementById(folderStack[i]);
+        element.outerHTML = "";
+        delete element;
+        var elephant = document.getElementById(folderStack[i] + "bar");
+        elephant.outerHTML = "";
+        delete elephant;
+        elementIcon.src = "img/folder.png";
+      }
+      for (var i = pos; i < len; i++) {
+        folderStack.pop();
+      }
+    } else {
+      elementIcon.src = "img/openFolder.png";
+      folderStack.push(elementIcon.parentElement.parentElement.id + "/" + mediaItem.name);
+      addNavBar(elementIcon.parentElement.parentElement.id + "/" + mediaItem.name);
+      getFilesInDir(mediaItem.contents, elementIcon.parentElement.parentElement.id + "/" + mediaItem.name);
+    }
+  } else {
+    switch (getExt(mediaItem)) {
+      case ".ogg":
+      case ".mp3":
+        renderAudio(mediaItem, title);
+        break;
+      case ".pdf":
+        renderPdf(mediaItem, title);
+        break;
+      case ".webm":
+      case ".mp4":
+        renderVideo(mediaItem, title);
+        break;
+      case ".png":
+      case ".jpg":
+        renderPhoto(mediaItem, title);
+        break;
+      case ".txt":
+        renderText(mediaItem, title);
+        mediaSrc = "img/textFile.png";
+        break;
+        //case "tv":
+        // break;
+      default:
+        mediaSrc = "img/unreadable.png";
+        break;
+    }
   }
-  return createIconSpan(faIconClass);
 }
 
-function getCategoryIcon(mediaItem) {
-  var faIconClass;
-  switch (mediaItem.value.toString().toLowerCase()) {
-    case "videos":
-      faIconClass = "fa-film";
-      break;
-    case "documents":
-      faIconClass = "fa-file-o";
-      break;
-    case "books":
-      faIconClass = "fa-book";
-      break;
-    case "music":
-      faIconClass = "fa-music";
-      break;
-    case "tv":
-      faIconClass = "fa-desktop"; // webix does not support fa-tv yet.
-      break;
-    case "photos":
-      faIconClass = "fa-image";
-      break;
-    default:
-      faIconClass = "fa-github";
-      break;
-  }
-  return createIconSpan(faIconClass);
+function getCurrentDir(title) {
+  return title.substring(title.lastIndexOf("/") + 1);
 }
 
-/*
- * Load media_list from the json file
- */
-$.getJSON("data/media_files_list.json", function(result) {
-  // keep the retrieved data in a cache for later use.
-  // cacheImageFilePaths(result);
+function addNavBar(name) {
+  var nam = name + "bar";
+  var nav = $("<li></li>", {
+    "class": "",
+    "id": nam
+  });
+  var txt = $("<a></a>", {
+    "href": "#" + name
+  });
+  txt.html(getCurrentDir(name));
+  nav.append(txt);
+  nav.appendTo("#navBar");
+}
 
-  console.log(result);
-  // onSelectChange: function() {
-  //   var mediaItem = this.getSelectedItem();
-  //   if (mediaItem == undefined) {
-  //     // Case unselectAll event happens.
-  //     return;
-  //   }
-  //   if (mediaItem.category == 'videos' || mediaItem.category == 'tv') {
-  //     renderVideo(mediaItem);
-  //   } else if (mediaItem.category == 'documents' || mediaItem.category ==
-  //     'books') {
-  //     renderDoc(mediaItem);
-  //   } else if (mediaItem.category == 'music') {
-  //     renderAudio(mediaItem);
-  //   } else if (mediaItem.category == 'photos') {
-  //     renderPhoto(mediaItem);
-  //     // webix.message("Feature is coming soon!");
-  //   } else {
-  //     webix.message("Unsupported type" + mediaItem.category);
-  //   }
-  //   $$(this).unselectAll();
-  // }
+function getFilesInDir(dir, title) {
+  var mediaDirs = jsonResult;
+  var currentDir = getCurrentDir(title);
+  var directory = $("<div></div>", {
+    "id": title,
+  });
+  directory.appendTo("#files_list");
+  var name = $("<h2>" + currentDir + "</h2>", {});
+  name.appendTo(directory);
+  $.each(dir, function(i, mediaItem) {
+    var mediaSrc = getMediaFilePath(mediaItem, title);
+    var divType = "img";
+    var modalType = "";
+    if (mediaItem.type == "directory") {
+      mediaSrc = "img/folder.png";
+    } else {
+      switch (getExt(mediaItem)) {
+        //   case "document":
+        //  mediaSrc = mediaItem;
+        //break;
+        case ".ogg":
+        case ".mp3":
+          mediaSrc = "img/musicFile.png";
+          modalType = "audio";
+          break;
+        case ".pdf":
+          mediaSrc = "img/pdfFile.png";
+          modalType = "pdf";
+          break;
+        case ".webm":
+        case ".mp4":
+          modalType = "video";
+          divType = "video";
+          break;
+        case ".png":
+        case ".jpg":
+          modalType = "img";
+          break;
+        case ".txt":
+          mediaSrc = "img/textFile.png";
+          modalType = "pdf";
+          break;
+          //case "tv":
+          // break;
+        default:
+          mediaSrc = "img/unreadable.png";
+          break;
+      }
+    }
+    var img = $("<div></div>", {
+      "class": "thumbContainer"
+    });
+    var image = $("<" + divType + "></" + divType + ">", {
+      "id": mediaItem.name + "Icon",
+      "class": "bubble_thumb",
+      "data-toggle": "modal",
+      "data-target": "#" + modalType + "_modal",
+      "value": mediaItem,
+      "src": mediaSrc
+    });
+    var lnk = $("<a></a>", {
+      "class": "fileName",
+      "data-toggle": "modal",
+      "data-target": "#" + modalType + "_modal",
+      "value": mediaItem
+    });
+    lnk.html(mediaItem.name);
+    lnk.on("click", function() {
+      renderMediaDynamic(mediaItem, title);
+    });
+    image.html(mediaItem.name);
+    image.on("click", function() {
+      renderMediaDynamic(mediaItem, title);
+    });
+    img.append(image);
+    img.append(lnk);
+    img.appendTo(directory);
+  });
+}
+$.getJSON("data/media_files_list_v3.json", function(result) {
+  jsonResult = result;
+  addNavBar("")
+  getFilesInDir(result, "");
 }); // end of getJSON()
+$("#audio_modal").on("hide.bs.modal", function(e) {
+  $("#audio_elm")[0].pause();
+});
+$("#video_modal").on("hide.bs.modal", function(e) {
+  $("#video_elm")[0].pause();
+});
